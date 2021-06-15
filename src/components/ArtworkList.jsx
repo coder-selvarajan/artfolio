a8d3ed295a087486c65e6751b14efe935fe6be06
Selvarajan Thangavel <selvarajan.thangavel@d4t4solutions.com>
2021-06-15 10:13:33 +0530

    Adding title to the artwork

--- src/components/ArtworkList.jsx
+++ src/components/ArtworkList.jsx
@@ -2,6 +2,7 @@ import Box from "@material-ui/core/Box";
 import ImageList from "@material-ui/core/ImageList";
 import ImageListItem from "@material-ui/core/ImageListItem";
 import Artwork from "./Artwork";
+import ImageListItemBar from "@material-ui/core/ImageListItemBar";
 
 const ArtworkList = (props) => {
   const { filteredPics } = props;
@@ -12,6 +13,16 @@ const ArtworkList = (props) => {
           filteredPics.map((item) => (
             <ImageListItem key={item.img}>
               <Artwork item={item} />
+              <ImageListItemBar
+                sx={{
+                  background:
+                    "linear-gradient(to top, rgba(0,0,0,0.4) 0%, " +
+                    "rgba(0,0,0,0.2) 50%, rgba(0,0,0,0) 100%)",
+                }}
+                title={item.title}
+                position='bottom'
+                actionPosition='left'
+              />
             </ImageListItem>
           ))}
       </ImageList>

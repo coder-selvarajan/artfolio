e47994f5101b52f7d155be2e8a29bcb5df7571e3
Selvarajan Thangavel <selvarajan.thangavel@d4t4solutions.com>
2021-06-16 10:39:07 +0530

    Build fix

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,19 +1,9 @@
-import Toolbar from "@material-ui/core/Toolbar";
-import { makeStyles } from "@material-ui/styles";
 import { categories } from "../data/constants";
 import Chip from "@material-ui/core/Chip";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import Box from "@material-ui/core/Box";
 
-const useStyles = makeStyles((theme) => ({
-  toolbarSecondary: {
-    justifyContent: "space-between",
-    overflowX: "auto",
-  },
-}));
-
 const FilterBar = (props) => {
-  const classes = useStyles();
   const { handleFilterClick, selectedFilter } = props;
 
   return (
